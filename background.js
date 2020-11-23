fa208cd09d752fce0986467762a0e9b58627dfe6
aviyam <aviyam@gmail.com>
2020-11-23 21:23:09 +0200

    removed console writing

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 	
   chrome.webRequest.onBeforeSendHeaders.addListener(
         function(details) {
-		  console.log('----harpagon---');
+		 
           for (var i = 0; i < details.requestHeaders.length; ++i) {
             if (details.requestHeaders[i].name === 'User-Agent') {
               details.requestHeaders[i].value = 'Mozila/5.0(Linux; Android 5.0; SM-g920A) AppleWebKit(KHTMK, like Gecko) Chrome Mobile Safari(compatible; AdsBot-Google-Mobile; +http://www.google.com/mobile/adsbot.html)';
