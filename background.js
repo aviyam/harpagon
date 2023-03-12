936010f0ecf5982e4c7d35088bae1ffc0983eea8
Aviyam Fischer <aviyam@gmail.com>
2023-03-12 07:50:15 +0200

    Changed bot due to blockage of googlebot

--- background.js
+++ background.js
@@ -4,7 +4,7 @@
 		 
           for (var i = 0; i < details.requestHeaders.length; ++i) {
             if (details.requestHeaders[i].name === 'User-Agent') {
-              details.requestHeaders[i].value = 'Mozila/5.0(Linux; Android 5.0; SM-g920A) AppleWebKit(KHTMK, like Gecko) Chrome Mobile Safari(compatible; AdsBot-Google-Mobile; +http://www.google.com/mobile/adsbot.html)';
+              details.requestHeaders[i].value = 'facebookexternalhit/1.1 +http://www.facebook.com/externalhit_uatext.php';
 			  
             }
           }
